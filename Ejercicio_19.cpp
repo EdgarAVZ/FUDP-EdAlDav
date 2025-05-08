5ab1ec8fd2837f65c04f991191b88ea9fe6fc658
EdgarAVZ <00210925@uca.edu.sv>
2025-05-08 11:49:52 -0600

    ej19

--- Ejercicio_19.cpp
+++ Ejercicio_19.cpp
@@ -6,18 +6,18 @@ int main(){
     cout << "Ingrese el consumo en Kwh: ";
     cin >> consumo;
 
-    if (consumo < 100){
+    if (consumo < 100 && consumo > 0){
         cout << "Usted tiene la tarifa menor" << endl;
 
     }else if(consumo >= 100 && consumo <= 200){
         cout << "Usted tiene la tarifa media" << endl;
 
     }else if(consumo > 200){
-        cout << "Usted tiene la tarifa alta" << endl;
-
-    }else{
-        cout << "Ingrese un numero valido" << endl;
-    }
+        cout <<"Usted tiene la tarifa alta" << endl;
     
+    }else {
+        cout << "Ingrese un consumo valido" << endl;
+    }
+
     return 0;
 }
